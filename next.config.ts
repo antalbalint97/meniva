18020e657a1bcd0b8e6e9b2589e79974d1732625
Bálint Antal <antalbalint97@gmail.com>
2025-10-07 22:22:12 +0200

    Contact + Subscribe fix v0.2

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,11 @@
 import type { NextConfig } from "next";
 import createMDX from "@next/mdx";
 
-const withMDX = createMDX({
-  extension: /\.mdx?$/,
-});
+const withMDX = createMDX({ extension: /\.mdx?$/ });
 
 const nextConfig: NextConfig = {
   pageExtensions: ["ts", "tsx", "mdx"],
   reactStrictMode: true,
-  // optional: disable edge runtime to force Node.js environment for Nodemailer
-  experimental: {
-    serverComponentsExternalPackages: ["nodemailer"],
-  },
 };
 
 export default withMDX(nextConfig);
